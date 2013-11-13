97396af6eb29d4e531c506348d68352c47ce6eeb
n8fr8 <nathan@freitas.net>
2013-11-13 15:57:01 -0500

    makes default configuration to localhost

--- conf.py
+++ conf.py
@@ -1,7 +1,10 @@
-uurl = "http://192.168.1.112:8080/"
+uurl = "http://localhost:8080/"
 use_ssl = False
 auth_root = "auth/"
 cert_file = "unveillance_web.crt"
 key_file = "unveillance_web.key"
 assets_path = ""
 server_port = 8888
+cookie_tag = "foo"
+cookie_secret = "bad"
+no_access_cookie = "leave"
